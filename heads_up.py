2a9e0ea2e5bb80927b2ec3bbe6b143326b5f1b35
bbrianb <briban17@gmail.com>
2025-06-10 21:44:38 -0400

    there's probably hella problems but at least i maybe have quads worked in

--- heads_up.py
+++ heads_up.py
@@ -30,6 +30,7 @@ class Card:
     def __add__(self, other):
         return Card(self.rank+other, self.suit)
 
+
 class Rank:
     def __init__(self, rank):
         if isinstance(rank, int):
@@ -79,6 +80,7 @@ class Rank:
             return self.rankNumber > other.rankNumber
         return False
 
+
 class Deck:
     def __init__(self):
         self.cards = []
@@ -114,6 +116,7 @@ class Deck:
     def count(self, thing):
         return self.cards.count(thing)
 
+
 class Hand:
     def __init__(self):
         self.cards = []
@@ -133,23 +136,33 @@ class Hand:
     def __format__(self, format_spec):
         return f'{f'{str(self)}':{format_spec}}'
 
+
 class HandStrength(Enum):
+    FOUR_OF_A_KIND = auto()
     STRAIGHT_FLUSH = auto()
     ROYAL_FLUSH = auto()
 
+
 class Claim:
-    def __init__(self, cards, hand_strength, belongs_to_hand, high_card_info):
+    def __init__(self, cards, hand_strength, belongs_to_hand, high_card_info, original_claim):
         self.cards = cards
         self.handStrength = hand_strength
         self.belongsToHand = belongs_to_hand
         self.highCardInfo: list[Rank] = high_card_info
+        self.originalClaim = original_claim
         self.blocked = 0
 
     def __repr__(self):
-        return str([self.cards, self.handStrength, self.belongsToHand, self.highCardInfo, self.blocked])
+        return str([self.cards, self.handStrength, self.highCardInfo, self.belongsToHand, self.blocked])
 
     def __len__(self):
         return len(self.cards)
+
+    def __eq__(self, other):
+        if isinstance(other, Claim):
+            return self.cards == other.cards and self.belongsToHand == other.belongsToHand and self.highCardInfo == other.highCardInfo
+        else:
+            return False
 
 
 def equity(hands, deck, community_cards=None):
@@ -157,66 +170,16 @@ def equity(hands, deck, community_cards=None):
         community_cards = []
     to_be_dealt = 5 - len(community_cards)
 
-    all_claims = get_claims(community_cards, deck, hands, to_be_dealt)
-    process_claims(all_claims, deck, to_be_dealt)
+    all_claims = get_claims(community_cards, deck, hands)
+    process_claims(all_claims, hands, deck, to_be_dealt)
 
 
-def process_claims(all_claims, deck, to_be_dealt):
-    print(all_claims)
-    i = 0
-    while i in range(len(all_claims) - 1):
-        current_claim = all_claims[i]
-        j = 0
-        while j in range(len(all_claims[i + 1:])):
-            k = i + 1 + j
-            other_claim = all_claims[k]
-            overlapping = sum(1 for card in other_claim.cards if card in current_claim.cards)
-
-            # the same exact claim --> one claim has to lose, so it will get deleted
-            if overlapping == len(current_claim):
-
-                if current_claim.handStrength == other_claim.handStrength:
-                    if current_claim.handStrength in (HandStrength.STRAIGHT_FLUSH, HandStrength.ROYAL_FLUSH):
-                        if current_claim.highCardInfo[0] > other_claim.highCardInfo[0]:
-                            all_claims.pop(k)
-                            j -= 1
-                        else:
-                            all_claims.pop(i)
-                            i -= 1
-                else:
-                    pass
-                    # the higher hand automatically beats out the other one every time
-
-            elif overlapping != 0:
-                if current_claim.handStrength == other_claim.handStrength:
-                    if current_claim.handStrength in (HandStrength.STRAIGHT_FLUSH, HandStrength.ROYAL_FLUSH):
-                        if current_claim.highCardInfo[0] > other_claim.highCardInfo[0]:
-                            stronger = i
-                            weaker = k
-                        else:
-                            stronger = k
-                            weaker = i
-
-                        weaker_length = len(all_claims[weaker])
-                        needed_for_block = len(all_claims[stronger]) - overlapping
-                        new_deck = len(deck) - weaker_length - needed_for_block
-                        remaining = max(to_be_dealt - weaker_length - needed_for_block, 0)
-
-                        if needed_for_block <= to_be_dealt - weaker_length:
-                            all_claims[weaker].blocked += math.comb(weaker_length, weaker_length) * math.comb(needed_for_block, needed_for_block) * math.comb(new_deck, remaining)
-                            print(all_claims[weaker], all_claims[stronger], overlapping)
-                else:
-                    pass
-
-            j += 1
-        i += 1
-    print(all_claims)
-
-
-def get_claims(community_cards, deck, hands, to_be_dealt):
+def get_claims(community_cards, deck, hands):
     for hand in hands:
         hand.winning_runouts = 0
-    all_claims = []
+
+    potential_claims = []
+
     # straight flush
     for suit in ('s', 'h', 'c', 'd'):
         for low_end in range(10, 0, -1):
@@ -229,17 +192,115 @@ def get_claims(community_cards, deck, hands, to_be_dealt):
                     if card in current_straight:
                         potential_claim.remove(card)
 
-                for card in potential_claim:
-                    if card not in deck.cards:
-                        break
+                if low_end == 10:
+                    hand_strength = HandStrength.ROYAL_FLUSH
                 else:
-                    if low_end == 10:
-                        hand_strength = HandStrength.ROYAL_FLUSH
-                    else:
-                        hand_strength = HandStrength.STRAIGHT_FLUSH
-                    if len(potential_claim) <= to_be_dealt and len(potential_claim) < 5:
-                        all_claims.append(Claim(potential_claim, hand_strength, current_hand, [Rank(low_end+4)]))
+                    hand_strength = HandStrength.STRAIGHT_FLUSH
+
+                potential_claims.append(Claim(potential_claim, hand_strength, current_hand, [Rank(low_end+4)], current_straight))
+
+    # quads
+    for current_rank in range(14, 1, -1):
+        for kicker in range(14, 1, -1):
+            if kicker != current_rank:
+                for current_suit in ('s', 'h', 'c', 'd'):
+                    current_quads = [Card(current_rank, suit) for suit in ('s', 'h', 'c', 'd')] + [Card(kicker, current_suit)]
+                    for current_hand in hands:
+                        potential_claim = current_quads.copy()
+
+                        for card in current_hand.cards + community_cards:
+                            if card in current_quads:
+                                potential_claim.remove(card)
+
+                        potential_claims.append(Claim(potential_claim, HandStrength.FOUR_OF_A_KIND, current_hand, [Rank(current_rank), Rank(kicker)], current_quads))
+
+    all_claims = []
+    for potential_claim in potential_claims:
+        for card in potential_claim.cards:
+            if card not in deck.cards:
+                break
+        else:
+            if len(potential_claim) < len(potential_claim.originalClaim) and all_claims.count(potential_claim) == 0:
+                all_claims.append(potential_claim)
+
     return all_claims
+
+
+# noinspection PyUnusedLocal
+def process_claims(all_claims, hands, deck, to_be_dealt):
+    print(all_claims)
+    current_index = 0
+    while current_index in range(len(all_claims) - 1):
+        j = 0
+        while j in range(len(all_claims[current_index + 1:])):
+            current_claim = all_claims[current_index]
+            other_index = current_index + 1 + j
+            other_claim = all_claims[other_index]
+            overlapping = sum(1 for card in other_claim.cards if card in current_claim.cards)
+
+            if overlapping == len(current_claim) or overlapping == len(other_claim):
+                stronger_hand = determine_stronger_hand(current_claim, other_claim)
+                print(current_claim, other_claim, stronger_hand)
+
+                if stronger_hand in ('chop', 'current'):
+                    all_claims.pop(other_index)
+                if stronger_hand in ('chop', 'other'):
+                    all_claims.pop(current_index)
+                    current_index -= 1
+                j -= 1
+
+            elif overlapping != 0:
+                stronger_hand = determine_stronger_hand(current_claim, other_claim)
+
+                if stronger_hand == 'current':
+                    stronger = current_index
+                    weaker = other_index
+                else:
+                    stronger = other_index
+                    weaker = current_index
+
+                weaker_length = len(all_claims[weaker])
+                needed_for_block = len(all_claims[stronger]) - overlapping
+                new_deck_length = len(deck) - weaker_length - needed_for_block
+                remaining = max(to_be_dealt - weaker_length - needed_for_block, 0)
+
+                if needed_for_block <= to_be_dealt - weaker_length:
+                    all_claims[weaker].blocked += math.comb(new_deck_length, remaining)
+
+            j += 1
+        current_index += 1
+
+    print(all_claims)
+
+    for claim in all_claims:
+        claim_length = len(claim)
+        remaining = to_be_dealt - claim_length
+        new_deck_length = len(deck) - claim_length
+
+        claim.belongsToHand.winning_runouts += math.comb(new_deck_length, remaining) - claim.blocked
+
+    for hand in hands:
+        hand.equity = hand.winning_runouts / math.comb(len(deck), to_be_dealt)
+        print(hand, hand.winning_runouts, hand.equity)
+
+
+def determine_stronger_hand(current_claim, other_claim):
+    if current_claim.handStrength == other_claim.handStrength:
+        return compare_high_cards(current_claim.highCardInfo, other_claim.highCardInfo)
+    elif current_claim.handStrength.value > other_claim.handStrength.value:
+        return 'current'
+    else:
+        return 'other'
+
+
+def compare_high_cards(current_high_cards, other_high_cards):
+    for current_info, other_info in zip(current_high_cards, other_high_cards):
+        if current_info > other_info:
+            return 'current'
+        elif other_info > current_info:
+            return 'other'
+    else:
+        return 'chop'
 
 
 def main():
@@ -248,10 +309,18 @@ def main():
 
     deck.shuffle()
     player1, player2 = Hand(), Hand()
-    deck.deal(player1, '2', 's')
-    deck.deal(player1, '3', 's')
-    deck.deal(player2, '7', 's')
-    deck.deal(player2)
+    players = player1, player2
+
+    cheating = True
+    if cheating:
+        deck.deal(player1, '5', 's')
+        deck.deal(player1, 'K', 'h')
+        deck.deal(player2, '7', 'h')
+        deck.deal(player2, 'Q', 'h')
+    else:
+        for _ in range(2):
+            for player in players:
+                deck.deal(player)
 
     equity((player1, player2), deck)
 
